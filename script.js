d2694db5c576f7564075992b21b760e8f2e3465c
Fonkeyy <113917387+Fonkeyy@users.noreply.github.com>
2023-12-25 20:20:12 +0100

    .

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+// const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+// const paragraphs = document.querySelectorAll('p');
+
+// paragraphs.forEach(
+//     (p) =>
+//         (p.onclick = (event) => {
+//             let iterations = 0;
+//             const length = event.target.dataset.value.length;
+
+//             const interval = setInterval(() => {
+//                 event.target.innerText = event.target.innerText
+//                     .slice(0, length)
+//                     .split('')
+//                     .map((letter, index) => {
+//                         if (index < iterations) {
+//                             return event.target.dataset.value[index];
+//                         }
+//                         return letters[Math.floor(Math.random() * 26)];
+//                     })
+//                     .join('');
+
+//                 if (iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+//                 iterations += 1 / 3;
+//             }, 30);
+//         })
+// );
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-document.querySelector('h1').onmouseover = (event) => {
+document.querySelector('p').onclick = (event) => {
     let iterations = 0;
 
     const interval = setInterval(() => {
@@ -16,6 +45,28 @@ document.querySelector('h1').onmouseover = (event) => {
 
         if (iterations >= event.target.dataset.value.length) clearInterval(interval);
 
-        iterations += 1 / 3;
+        iterations += 1 / 2;
     }, 30);
+};
+
+document.querySelector('.answer').onclick = (event) => {
+    let iterations = 0;
+    const length = event.target.dataset.value.length;
+
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+            .slice(0, length)
+            .split('')
+            .map((letter, index) => {
+                if (index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * 26)];
+            })
+            .join('');
+
+        if (iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+        iterations += 1 / 8;
+    }, 10);
 };
